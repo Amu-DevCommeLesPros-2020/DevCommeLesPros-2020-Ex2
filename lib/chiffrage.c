5d8c89bb3c88c2a38097473c1b2268d8eefe31e3
Thierry Seegers <surfintheusa@yahoo.com>
2019-11-26 18:03:57 +0100

    Introduce encryption library
    Introduce functions with empty implementations.

---
+++ lib/chiffrage.c
@@ -0,0 +1,53 @@
+#include "chiffrage.h"
+
+#include <stddef.h>
+
+char* chiffre_ROT13(char* clair)
+{
+    return NULL;
+}
+
+char* dechiffre_ROT13(char* chiffre)
+{
+    return NULL;
+}
+
+char* chiffre_Cesar(char* clair, char const cle)
+{
+    return NULL;
+}
+
+char* dechiffre_Cesar(char* chiffre, char const cle)
+{
+    return NULL;
+}
+
+char* chiffre_Vigenere(char* clair, char const* cle)
+{
+    return NULL;
+}
+
+char* dechiffre_Vigenere(char* chiffre, char const* cle)
+{
+    return NULL;
+}
+
+void chiffre_Vigenere_flux_texte(FILE* clair, FILE* chiffre, char const* cle)
+{
+    return;
+}
+
+void dechiffre_Vigenere_flux_texte(FILE* clair, FILE* chiffre, char const* cle)
+{
+    return;
+}
+
+void chiffre_Vigenere_flux_binaire(FILE* clair, FILE* chiffre, char const* cle)
+{
+    return;
+}
+
+void dechiffre_Vigenere_flux_binaire(FILE* clair, FILE* chiffre, char const* cle)
+{
+    return;
+}
