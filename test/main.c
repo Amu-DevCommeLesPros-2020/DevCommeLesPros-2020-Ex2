6247b823429012fc146c4df665a195707a7f99d5
Thierry Seegers <surfintheusa@yahoo.com>
2019-11-29 16:25:12 +0100

    Introduce tests for Ceasar encryption
    As is, the codebase will fail to compile because the functions (de)chiffre_Cesar are not linked. I'm tempted to leave it that way as the first thing that students will have to fix.

--- test/main.c
+++ test/main.c
@@ -20,8 +20,6 @@ int main()
     TEST_STR(chiffre_ROT13(clair), "n");
     strcpy(clair, "abcdefghijklmnopqrstuvwxyz");
     TEST_STR(chiffre_ROT13(clair), "nopqrstuvwxyzabcdefghijklm");
-    strcpy(clair, "aA");
-    TEST_STR(chiffre_ROT13(clair), "nN");
     strcpy(clair, "aBcdEFghiJKLmnopQRSTuVwXyZ");
     TEST_STR(chiffre_ROT13(clair), "nOpqRStuvWXYzabcDEFGhIjKlM");
     strcpy(clair, "A, a.");
@@ -40,6 +38,72 @@ int main()
     TEST_STR(dechiffre_ROT13(clair), "A, a.");
     strcpy(clair, "Wr ynvffr zrf ovraf à zn fbrhe ? Aba ! À zba arirh. Wnznvf fren cnlé yr pbzcgr qh gnvyyrhe. Evra nhk cnhierf.");
     TEST_STR(dechiffre_ROT13(clair), "Je laisse mes biens à ma soeur ? Non ! À mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+
+
+    // Test chiffre_Cesar.
+    strcpy(clair, "a");
+    TEST_STR(chiffre_Cesar(clair, 'a'), "a");
+    strcpy(clair, "a");
+    TEST_STR(chiffre_Cesar(clair, 'c'), "c");
+    strcpy(clair, "a");
+    TEST_STR(chiffre_Cesar(clair, 'z'), "z");
+    strcpy(clair, "abcdefghijklmnopqrstuvwxyz");
+    TEST_STR(chiffre_Cesar(clair, 'a'), "abcdefghijklmnopqrstuvwxyz");
+    strcpy(clair, "abcdefghijklmnopqrstuvwxyz");
+    TEST_STR(chiffre_Cesar(clair, 'c'), "cdefghijklmnopqrstuvwxyzab");
+    strcpy(clair, "abcdefghijklmnopqrstuvwxyz");
+    TEST_STR(chiffre_Cesar(clair, 'z'), "zabcdefghijklmnopqrstuvwxy");
+    strcpy(clair, "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    TEST_STR(chiffre_Cesar(clair, 'a'), "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    strcpy(clair, "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    TEST_STR(chiffre_Cesar(clair, 'c'), "cDefGHijkLMNopqrSTUVwXyZaB");
+    strcpy(clair, "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    TEST_STR(chiffre_Cesar(clair, 'z'), "zAbcDEfghIJKlmnoPQRStUvWxY");
+    strcpy(clair, "A, a.");
+    TEST_STR(chiffre_Cesar(clair, 'a'), "A, a.");
+    strcpy(clair, "A, a.");
+    TEST_STR(chiffre_Cesar(clair, 'c'), "C, c.");
+    strcpy(clair, "A, a.");
+    TEST_STR(chiffre_Cesar(clair, 'z'), "Z, z.");
+    strcpy(clair, "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    TEST_STR(chiffre_Cesar(clair, 'a'), "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    strcpy(clair, "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    TEST_STR(chiffre_Cesar(clair, 'c'), "Lg nckuug ogu dkgpu à oc uqgwt. Pqp à oqp pgxgw. Lcocku ugtc rcaé ng eqorvg fw vcknngwt. Tkgp cwz rcwxtgu.");
+    strcpy(clair, "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    TEST_STR(chiffre_Cesar(clair, 'z'), "Id kzhrrd ldr ahdmr à lz rndtq. Mnm à lnm mdudt. Izlzhr rdqz ozxé kd bnlosd ct szhkkdtq. Qhdm ztw oztuqdr.");
+
+    // Tests dechiffre_Cesar.
+    strcpy(clair, "a");
+    TEST_STR(dechiffre_Cesar(clair, 'a'), "a");
+    strcpy(clair, "c");
+    TEST_STR(dechiffre_Cesar(clair, 'c'), "a");
+    strcpy(clair, "z");
+    TEST_STR(dechiffre_Cesar(clair, 'z'), "a");
+    strcpy(clair, "abcdefghijklmnopqrstuvwxyz");
+    TEST_STR(dechiffre_Cesar(clair, 'a'), "abcdefghijklmnopqrstuvwxyz");
+    strcpy(clair, "cdefghijklmnopqrstuvwxyzab");
+    TEST_STR(dechiffre_Cesar(clair, 'c'), "abcdefghijklmnopqrstuvwxyz");
+    strcpy(clair, "zabcdefghijklmnopqrstuvwxy");
+    TEST_STR(dechiffre_Cesar(clair, 'z'), "abcdefghijklmnopqrstuvwxyz");
+    strcpy(clair, "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    TEST_STR(dechiffre_Cesar(clair, 'a'), "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    strcpy(clair, "cDefGHijkLMNopqrSTUVwXyZaB");
+    TEST_STR(dechiffre_Cesar(clair, 'c'), "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    strcpy(clair, "zAbcDEfghIJKlmnoPQRStUvWxY");
+    TEST_STR(dechiffre_Cesar(clair, 'z'), "aBcdEFghiJKLmnopQRSTuVwXyZ");
+    strcpy(clair, "A, a.");
+    TEST_STR(dechiffre_Cesar(clair, 'a'), "A, a.");
+    strcpy(clair, "C, c.");
+    TEST_STR(dechiffre_Cesar(clair, 'c'), "A, a.");
+    strcpy(clair, "Z, z.");
+    TEST_STR(dechiffre_Cesar(clair, 'z'), "A, a.");
+    strcpy(clair, "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    TEST_STR(dechiffre_Cesar(clair, 'a'), "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    strcpy(clair, "Lg nckuug ogu dkgpu à oc uqgwt. Pqp à oqp pgxgw. Lcocku ugtc rcaé ng eqorvg fw vcknngwt. Tkgp cwz rcwxtgu.");
+    TEST_STR(dechiffre_Cesar(clair, 'c'), "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+    strcpy(clair, "Id kzhrrd ldr ahdmr à lz rndtq. Mnm à lnm mdudt. Izlzhr rdqz ozxé kd bnlosd ct szhkkdtq. Qhdm ztw oztuqdr.");
+    TEST_STR(dechiffre_Cesar(clair, 'z'), "Je laisse mes biens à ma soeur. Non à mon neveu. Jamais sera payé le compte du tailleur. Rien aux pauvres.");
+
 
     return resultat;
 
